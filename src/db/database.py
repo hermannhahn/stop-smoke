85e03991b85e4779d30ee0d1d598fc0450a33a63
Hermann Hahn <hermann.h.hahn@gmail.com>
2023-03-08 22:45:09 -0300

    v1.5.1

--- src/db/database.py
+++ src/db/database.py
@@ -7,7 +7,7 @@ class SmokingDatabase:
 
     def __init__(self):
         """Connect to the database and create the table if it doesn't exist"""
-        self.conn = sqlite3.connect('../smoking.db')
+        self.conn = sqlite3.connect('stopsmoking.db')
         self.cursor = self.conn.cursor()
 
         self.cursor.execute('''CREATE TABLE IF NOT EXISTS smoking (
